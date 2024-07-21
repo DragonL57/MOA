c67f3d31e1f9089555173d12db081fd601fe8f68
DragonL57 <vmthelong2004@gmail.com>
2024-07-21 13:11:29 +0700

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Use raw string or forward slashes for the file path to avoid unicode errors
-cred = credentials.Certificate(r"C:\Users\vmthe\MOA-groq\moa-groq-firebase-adminsdk-qo6gc-827484e874.json")
+cred = credentials.Certificate(r".\moa-groq-firebase-adminsdk-qo6gc-4fbf31650a.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
