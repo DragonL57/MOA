4402d7478e3da60a547b4b2c02cbf18c1b6abf72
DragonL57 <vmthelong2004@gmail.com>
2024-07-22 01:11:17 +0700

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,8 +1,14 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Use raw string or forward slashes for the file path to avoid unicode errors
+# Path to your service account key file
 cred = credentials.Certificate(r"./moa-groq-firebase-adminsdk-qo6gc-827484e874.json")
-firebase_admin.initialize_app(cred)
 
+# Check if the app is already initialized
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
+else:
+    firebase_admin.initialize_app(cred, name='unique_app_name')
+
+# Firestore database instance
 db = firestore.client()
